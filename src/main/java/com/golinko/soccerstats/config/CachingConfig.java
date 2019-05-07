fbf1014b9a93aa5d687f974f4d52ef4e4e0a337f
Anna Okhrimenko <anna.golinko@gmail.com>
2019-05-08 00:46:46 +0200

    add caching for the actions list

---
+++ src/main/java/com/golinko/soccerstats/config/CachingConfig.java
@@ -0,0 +1,17 @@
+package com.golinko.soccerstats.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new EhCacheCacheManager();
+    }
+}
