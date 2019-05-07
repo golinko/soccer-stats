7658da94df09d133d450aba0b2a74a257c29dd8e
Anna Okhrimenko <anna.golinko@gmail.com>
2019-05-08 00:49:15 +0200

    configure cors allow only get methods

---
+++ src/main/java/com/golinko/soccerstats/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.golinko.soccerstats.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration class for web security related beans outside of the security configurer
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET");
+            }
+        };
+    }
+}
